a76394bed3a14745c9bc7e2725f2cb58277b6dc9
Mor Mauda <MorM@Mors-MacBook-Pro.local>
2016-03-11 00:31:27 +0200

    prepare surface for contact List

---
+++ app/src/main/java/m/mimavima/Contact.java
@@ -0,0 +1,17 @@
+package m.mimavima;
+
+/**
+ * Created by MorM on 3/11/16.
+ */
+public class Contact {
+
+    String contactName;
+    int contactId;
+
+    Contact(String name, int Id)
+    {
+        this.contactName = name;
+        this.contactId = Id;
+    }
+
+}
---
+++ app/src/main/java/m/mimavima/ContactListAdapter.java
@@ -0,0 +1,71 @@
+package m.mimavima;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by MorM on 3/10/16.
+ */
+public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ContactViewHolder>{
+
+        List<Contact> contacts;
+
+        public class ContactViewHolder extends RecyclerView.ViewHolder {
+
+            CardView cv;
+
+            TextView contactName;
+            TextView contactId;
+
+
+
+            ContactViewHolder(View itemView) {
+                super(itemView);
+                cv = (CardView)itemView.findViewById(R.id.cv);
+                contactName = (TextView)itemView.findViewById(R.id.contact_name);
+                contactId = (TextView)itemView.findViewById(R.id.contact_id);
+
+
+            }
+
+
+        }
+        //Constructor
+        ContactListAdapter(List<Contact> contacts){
+            this.contacts = contacts;
+        }
+
+        @Override
+        public ContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_card_fregment, parent, false);
+            ContactViewHolder fvh = new ContactViewHolder(v);
+            return fvh;
+
+        }
+
+        @Override
+        public void onBindViewHolder(ContactViewHolder fruitViewHolder, int i) {
+            fruitViewHolder.contactName.setText(contacts.get(i).contactName);
+//        System.out.println(fruits.get(i).num);
+            fruitViewHolder.contactId.setText("" + contacts.get(i).contactId);
+        }
+
+        @Override
+        public int getItemCount() {
+            return contacts.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+}
